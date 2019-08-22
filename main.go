440fdc3ce25df2448d4040e826e0712a11ea3a45
Raymond Chan <chanr32@gmail.com>
2019-08-22 14:17:52 -0400

    refactor code

--- main.go
+++ main.go
@@ -12,18 +12,19 @@ import (
   "sync"
 )
 
-// Connected clients map
-var connections = Connections{m: make(map[string]*User)}
-var serverVariables ServerVariable
+type Server struct {
+  Listener net.Listener
+  Logger *log.Logger
+  Connections *Connections
+}
+
+type Config struct {
+  Ip, Port, LogFile string
+}
 
 type Connections struct {
   m map[string]*User
   sync.RWMutex
-}
-
-type ServerVariable struct {
-  Port, Ip, LogFile string
-  Logger *log.Logger
 }
 
 type User struct {
@@ -31,108 +32,82 @@ type User struct {
   connection net.Conn
 }
 
-func setDefault() {
-  serverVariables.Ip = "localhost"
-  serverVariables.Port = "9000"
-  serverVariables.LogFile = "chat.log"
+func NewServer(listener net.Listener, logger *log.Logger) *Server {
+  return &Server{
+    Listener: listener,
+    Logger: logger,
+    Connections: &Connections{m: make(map[string]*User)},
+  }
 }
 
-func main() {
+func setDefault(config *Config) {
+  config.Ip = "localhost"
+  config.Port = "9000"
+  config.LogFile = "chat.log"
+}
 
-  // Read config from file
-  file, err := os.Open("env.json")
-  defer file.Close()
-  if err != nil {
-    fmt.Println("Warn: config file does not exist, falling back to default")
-    setDefault()
-  } else {
-    decoder := json.NewDecoder(file)
-    err := decoder.Decode(&serverVariables)
-    if err != nil {
-      fmt.Println("Warn: failed decoding json file, falling back to default")
-      setDefault()
-    }
-  }
-
-  // Start TCP server
-  ln, err := net.Listen("tcp", serverVariables.Ip + ":" + serverVariables.Port)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  defer ln.Close()
-
-  // Open/Create Log file
-  f, err := os.OpenFile(serverVariables.LogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-  if err != nil {
-  	fmt.Println(err)
-  }
-  defer f.Close()
-
-  serverVariables.Logger = log.New(f, "chat-app: ", log.LstdFlags)
-
+func (s *Server) Start() {
   // Accept connections
   for {
-    conn, err := ln.Accept()
+    conn, err := s.Listener.Accept()
     if err != nil {
       fmt.Println(err)
       continue
     }
 
-    go handleConnection(conn)
+    go s.handleConnection(conn)
   }
 }
 
-func handleUsername(c net.Conn) {
-  c.Write([]byte("Please enter a username: "))
-  netData, err := bufio.NewReader(c).ReadString('\n')
+func main() {
+  config := &Config{}
+
+  // Read config from file
+  configFile, err := os.Open("env.json")
+  defer configFile.Close()
+  if err != nil {
+    fmt.Println("Warn: config file does not exist, falling back to default")
+    setDefault(config)
+  } else {
+    decoder := json.NewDecoder(configFile)
+    err := decoder.Decode(config)
+    if err != nil {
+      fmt.Println("Warn: failed decoding json file, falling back to default")
+      setDefault(config)
+    }
+  }
+
+  // Open/Create Log file
+  logFile, err := os.OpenFile(config.LogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+  if err != nil {
+  	fmt.Println(err)
+  }
+  defer logFile.Close()
+
+  logger := log.New(logFile, "chat-app: ", log.LstdFlags)
+
+  // Create listener
+  listener, err := net.Listen("tcp", config.Ip + ":" + config.Port)
   if err != nil {
     fmt.Println(err)
     return
   }
+  defer listener.Close()
 
-  // Set username to data from client
-  username := strings.TrimSpace(string(netData))
-  var user = new(User)
-  user.username = username
-  user.connection = c
-
-  // Broadcast user entered or changed username
-  connections.RLock()
-  if _, ok := connections.m[c.RemoteAddr().String()]; ok {
-    broadcast(connections.m[c.RemoteAddr().String()].username + " has changed their username to " + username)
-  } else {
-    broadcast(username + " has entered.")
-  }
-  connections.RUnlock()
-
-  // Set user to connections map
-  connections.Lock()
-  connections.m[c.RemoteAddr().String()] = user
-  connections.Unlock()
+  // Start server
+  server := NewServer(listener, logger)
+  server.Start()
 }
 
-func handleConnection(c net.Conn) {
-  fmt.Printf("Serving %s\n", c.RemoteAddr().String())
-
-  // Ask user for username
-  handleUsername(c)
-
-  // Listen for data from client
+func (s *Server) listenForMessages(conn net.Conn) {
   for {
-    netData, err := bufio.NewReader(c).ReadString('\n')
+    text, err := s.readFromClient(conn)
     if err != nil {
-      fmt.Println(err)
       break
     }
+    address := conn.RemoteAddr().String()
+    username := s.getUsername(address)
 
-    var address = c.RemoteAddr().String()
-
-    connections.RLock()
-    var username = connections.m[address].username
-    connections.RUnlock()
-
-    text := strings.TrimSpace(string(netData))
     // Continue if text is empty
     if text == "" {
       continue
@@ -144,45 +119,101 @@ func handleConnection(c net.Conn) {
     if text == "-exit" {
       fmt.Printf("Removing %s\n", address)
 
-      connections.Lock()
-      delete(connections.m, address)
-      connections.Unlock()
-
-      broadcast(username + " has left.")
+      s.removeConnection(address)
       break
     }
 
     // Client request username change
     if text == "-cu" {
-      handleUsername(c)
+      s.handleUsername(conn)
       continue
     }
 
     // Broadcast message
-    broadcast(username + ": " + text)
+    s.broadcast(username + ": " + text)
   }
-  c.Close()
+  conn.Close()
 }
 
-func broadcast(msg string) {
+func (s *Server) handleConnection(conn net.Conn) {
+  fmt.Println("Serving " + conn.RemoteAddr().String())
+
+  s.handleUsername(conn)
+
+  go s.listenForMessages(conn)
+}
+
+func (s *Server) handleUsername(conn net.Conn) {
+  s.writeToClient(conn, "Please enter a username: ")
+  username, err := s.readFromClient(conn)
+  if err != nil {
+    return
+  }
+  user := &User{}
+  user.username = username
+  user.connection = conn
+
+  // Set user to connections map
+  address := conn.RemoteAddr().String()
+  s.addConnection(address, user)
+}
+
+func (s *Server) writeToClient(conn net.Conn, msg string) {
+  _, err := conn.Write([]byte(msg))
+  if err != nil {
+    fmt.Println("Could not write to " + conn.RemoteAddr().String() + ", removing client")
+    // Assuming connection lost, removing client from connections map
+    s.removeConnection(conn.RemoteAddr().String())
+    return
+  }
+}
+
+func (s *Server) readFromClient(conn net.Conn) (string, error) {
+  netData, err := bufio.NewReader(conn).ReadString('\n')
+  if err != nil {
+    fmt.Println("Could not read from " + conn.RemoteAddr().String() + ", removing client")
+    // Assuming connection lost, removing client from connections map
+    s.removeConnection(conn.RemoteAddr().String())
+    return "", err
+  }
+
+  return strings.TrimSpace(string(netData)), err
+}
+
+func (s *Server) broadcast(msg string) {
   currentTime := time.Now()
 
   // Write to each user connected
-  for address, user := range connections.m {
-    _, err := user.connection.Write([]byte(currentTime.Format("\n(Mon, Jan 2 2006 - 15:04pm)") + " " + msg + "\n\n"))
-    // Could not write to client
-    // Assuming connection dropped, remove user from connections map
-    if err != nil {
-      fmt.Println("Could not write to " + address + ", connection dropped.")
-      fmt.Println("Removing " + address)
-      connections.Lock()
-      delete(connections.m, address)
-      connections.Unlock()
-    }
+  for _, user := range s.Connections.m {
+    s.writeToClient(user.connection, currentTime.Format("\n(Mon, Jan 2 2006 - 15:04pm)") + " " + msg + "\n\n")
   }
 
-  // Skip writing to log when running tests
-  if os.Getenv("ENV") != "Test" {
-    serverVariables.Logger.Println(msg)
-  }
+  s.Logger.Println(msg)
+}
+
+func (s *Server) addConnection(address string, user *User) {
+  s.Connections.Lock()
+  s.Connections.m[address] = user
+  s.Connections.Unlock()
+
+  s.broadcast(user.username + " has entered.")
+}
+
+func (s *Server) removeConnection(address string) {
+  fmt.Println("Removing " + address)
+  username := s.getUsername(address)
+
+  s.Connections.Lock()
+  delete(s.Connections.m, address)
+  s.Connections.Unlock()
+
+  s.broadcast(username + " has left.")
+}
+
+func (s *Server) getUsername(address string) string {
+  s.Connections.RLock()
+  username := s.Connections.m[address].username
+  s.Connections.RUnlock()
+
+  return username
 }
